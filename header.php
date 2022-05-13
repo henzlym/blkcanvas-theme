97a8463d6d965bf7b19ddf7adb8840a8b7158700
henzlym <45774619+henzlym@users.noreply.github.com>
2022-05-12 22:13:14 -0400

    add this class

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
 </head>
 
 <body <?php body_class(); ?>>
-    <header id="masthead" class="header <?php echo blkcanvas_get_header_class(); ?>">
+    <header id="masthead" class="header <?php echo blkcanvas_get_header_class(); ?> andaddthis">
         <div class="blkcanvas-container">
             <?php blkcanvas_logo(); ?>
             <?php get_template_part('searchform'); ?>
